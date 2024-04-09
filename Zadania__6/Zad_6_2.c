6edc1172bcf0d4d5c3e49764e0a16a1f48683363
Kits00n_UE <mikolaj.tabaka@edu.uekat.pl>
2024-04-09 11:28:51 +0200

    buja

--- Zadania__6/Zad_6_2.c
+++ Zadania__6/Zad_6_2.c
@@ -1,3 +1,20 @@
-//
-// Created by phant on 09.04.2024.
-//
+#include <stdio.h>
+
+int main() {
+    // Zdefiniowanie i inicjalizacja tablicy
+    int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    // Zdefiniowanie wskaźnika do typu int i ustawienie go na adres pierwszego elementu tablicy
+    int* pointer = array;
+
+    // Przejście przez każdy element tablicy
+    for (int i = 0; i < 10; i++) {
+        // Wydrukowanie wartości elementu, do którego wskazuje wskaźnik
+        printf("%d ", *pointer);
+
+        // Inkrementacja wskaźnika, aby wskazywał na następny element tablicy
+        pointer++;
+    }
+
+    return 0;
+}
